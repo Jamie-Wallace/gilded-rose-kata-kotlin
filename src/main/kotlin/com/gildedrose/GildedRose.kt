f3229d15035df123a8e75b1eda0196e7af4d7cd1
Jamie Wallace <jamie.wallace@mkodo.com>
2019-06-27 16:41:45 +0100

    Isolated Sulfuras item in if statement

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -41,26 +41,21 @@ class GildedRose(var items: Array<Item>) {
             if (item.sellIn < 0) {
                 item.quality = 0
             }
+        } else if (item.name == "Sulfuras, Hand of Ragnaros") {
+
         } else {
             if (item.quality > 0) {
-                if (item.name != "Sulfuras, Hand of Ragnaros") {
-                    item.quality = item.quality - 1
-                }
+                item.quality = item.quality - 1
             }
-
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.sellIn = item.sellIn - 1
-            }
+            item.sellIn = item.sellIn - 1
 
             if (item.sellIn < 0) {
                 if (item.quality > 0) {
-                    if (item.name != "Sulfuras, Hand of Ragnaros") {
-                        item.quality = item.quality - 1
-                    }
+                    item.quality = item.quality - 1
                 }
-
             }
         }
     }
 }
+
 
